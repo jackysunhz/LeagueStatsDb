1f7c7e0833133dbf2f93f2b90ba92511e50dc400
Jacky Sun <jackysunhz@126.com>
2019-11-30 16:25:21 -0600

    simple front end

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php include "templates/header.php"; ?>
+
+<ul>
+    <li><a href="lookup.php"><strong>Look Up</strong></a> - find the records for a player</li>
+	<li><a href="add.php"><strong>Add Player</strong></a> - add a player's records to the database</li>
+	<li><a href="update.php"><strong>Update Player</strong></a> - edit the records of existing player</li>
+	<li><a href="delete.php"><strong>Delete Player</strong></a> - delete a player</li>
+</ul>
+
+<?php include "templates/footer.php"; ?>
